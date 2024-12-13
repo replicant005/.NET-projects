c51c5629267e7adc8458b4defaaa0dff3b37ca62
Mehak Kapur <150804292+replicant005@users.noreply.github.com>
2024-12-13 13:13:23 -0500

    Add files via upload

---
+++ Exploring Blazor/.NET assignment COSC 2100/Questions.cs
@@ -0,0 +1,15 @@
+﻿/* Author - mehak kapur 
+ * Date - 7/12/2024
+ * Description : Class for questions
+ */
+
+namespace PersonalityTestFinal
+{
+    public class Questions
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public List<string> Options { get; set; }
+        public string SelectedOption { get; set; }
+    }
+}
